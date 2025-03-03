941de0bbe96b6a703e31da1b393581a72237558e
Ritika Patel <91361189+Riti-Patel@users.noreply.github.com>
2025-03-03 11:13:06 +0530

    Update storageUpload_file.py

--- snippet/storageUpload_file.py
+++ snippet/storageUpload_file.py
@@ -1,1 +1,35 @@
+import sys
 
+# [START storage_upload_file]
+from google.cloud import storage
+
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    
+    generation_match_precondition = 0
+
+    blob.upload_from_filename(source_file_name, if_generation_match=generation_match_precondition)
+
+    print(
+        f"File {source_file_name} uploaded to {destination_blob_name}."
+    )
+
+
+# [END storage_upload_file]
+
+if __name__ == "__main__":
+    upload_blob(
+        bucket_name=sys.bucket_labs-1,
+        source_file_name=sys."C:\Users\Sadu\Downloads",
+        destination_blob_name=sys."Image.JPG",
+    )

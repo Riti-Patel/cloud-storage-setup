33d8bc590ba96bf87b5156304bf8eb8fa08b9145
Ritika Patel <91361189+Riti-Patel@users.noreply.github.com>
2025-03-03 11:31:57 +0530

    Update storageList_files_bucket.py

--- snippet/storageList_files_bucket.py
+++ snippet/storageList_files_bucket.py
@@ -1,1 +1,11 @@
+def list_files(bucket_name):
+    """Lists all files in the specified bucket."""
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
 
+    blobs = bucket.list_blobs()
+    for blob in blobs:
+        print(blob.name)
+
+# Example Usage
+list_files("bucket_labs-1")
